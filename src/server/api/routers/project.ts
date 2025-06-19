615c0eb74d9104291b304bde9504bd64fd38e8f7
Fremdder <Srijanjain2004@gmail.com>
2025-06-19 18:21:38 +0530

    created function to genrate vector embedding of the files of repo

--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 import { pollCommits } from "@/lib/github";
+import { indexGithubRepo } from "@/lib/github-loader";
+import { Prisma } from "@prisma/client";
 
 export const projectRouter = createTRPCRouter({
 
@@ -11,19 +13,31 @@ export const projectRouter = createTRPCRouter({
             githubToken : z.string().optional()
         })
     ).mutation(async ({ctx,input}) => {
-        const project = await ctx.db.project.create({
-            data:{
-                githubUrl : input.githubUrl,
-                name : input.name,
-                userToProject : {
-                    create:{
-                        userId : ctx.user.userId!,
+        try{
+            const project = await ctx.db.project.create({
+                data:{
+                    githubUrl : input.githubUrl,
+                    name : input.name,
+                    userToProject : {
+                        create:{
+                            userId : ctx.user.userId!,
+                        }
                     }
                 }
-            }
-        })
-        await pollCommits(project.id)
-        return project
+            })
+            await indexGithubRepo(project.id,input.githubUrl,input.githubToken)
+            await pollCommits(project.id)
+            return project
+        }catch(error){
+            if (
+                error instanceof Prisma.PrismaClientKnownRequestError &&
+                error.code === "P2002"
+              ) {
+                throw new Error("A project with this name and GitHub URL already exists.");
+              }
+        
+              throw error;
+        }
     }),
     getProjects : protectedProcedure.query(async ({ctx}) => {
         return await ctx.db.project.findMany({
